c6e4f964990e2fce886987e84a053e99542dc666
Roshan Bhatia <37065373+2kwattz@users.noreply.github.com>
2023-05-07 14:53:41 +0530

    Add files via upload

--- Temperature Converter Task 3/js/index.js
+++ Temperature Converter Task 3/js/index.js
@@ -1,1 +1,13 @@
+function convertToFar() {
+    var celsiusInput = document.getElementById("celsius").value;
+    var fahrenheitOutput = (celsiusInput * 1.8) + 32;
+    document.getElementById("fahrenheit").value = fahrenheitOutput;
+    
+}
 
+function convertToCel() {
+    var fahrenheitInput = document.getElementById("fahrenheit").value;
+    var celsiusOutput = (fahrenheitInput - 32) / 1.8;
+    document.getElementById("celsius").value = celsiusOutput;
+    
+}
